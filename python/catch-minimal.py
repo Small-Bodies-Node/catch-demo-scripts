9138b13767544a56e331348143761aa1f39a5aea
Michael S. P. Kelley <msk@astro.umd.edu>
2022-02-01 10:30:00 -0500

    Ignore messages for other jobs.

--- python/catch-minimal.py
+++ python/catch-minimal.py
@@ -25,7 +25,7 @@ params = {
 }
 
 # API route is .../query/moving
-res = requests.get('https://catch.astro.umd.edu/catch/query/moving',
+res = requests.get('https://catch.astro.umd.edu/api/query/moving',
                    params=params)
 
 # response is JSON formatted
@@ -34,7 +34,7 @@ data = res.json()
 # If 'queued' is True, listen to the CATCH event stream until a message
 # with our job ID prefix (first 8 characters) is 'success' or 'error'.
 if data['queued']:
-    messages = SSEClient('https://catch.astro.umd.edu/catch/stream')
+    messages = SSEClient('https://catch.astro.umd.edu/api/stream')
     for message in messages:
         message_data = json.loads(message.data)
 
@@ -45,6 +45,8 @@ if data['queued']:
         if message_data['job_prefix'] == data['job_id'][:8]:
             # this message is for us, print the text
             print(message_data['text'], file=sys.stderr)
+        else:
+            continue
 
         # Message status may be 'success', 'error', 'running', 'queued'.
         if message_data['status'] in ['error', 'success']:
