a6907eea9d3cb91400cba3b2c8e72f819ea7975e
Michael S. P. Kelley <msk@astro.umd.edu>
2023-03-06 13:18:33 -0500

    Update minimal Python script.

--- python/catch-minimal.py
+++ python/catch-minimal.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Minimal Python script for moving target queries with the CATCH API.
+Minimal Python script for moving target queries with the CATCH API v2.0.
 
 CATCH is a search tool for finding comets and asteroids in NEO and time-domain
 survey data, hosted by the Planetary Data System's Small Bodies Node:
@@ -9,24 +9,30 @@ https://catch.astro.umd.edu/
 
 Requires: requests, sseclient
 
-(requests and sseclient greatly simplifies listening to the CATCH event stream)
+OK, not a strictly minimal script, but requests and sseclient greatly simplify
+listening to the CATCH event stream.
 
 """
+
 import sys
 import json
 import requests
 from sseclient import SSEClient
 
-# set up parameters, we are searching for comet 65P and do not want cached
-# results
+# Set up parameters:
+#   * search for comet 65P
+#   * do not retrieve cached results, but run a new search on the database
+# See https://catch.astro.umd.edu/apis for other options.
 params = {
-    'target': '65P',
-    'cached': 'false'
+    "target": "65P",
+    "sources": "neat_palomar_tricam",
+    "cached": "false"
 }
 
-# API route is .../query/moving
-res = requests.get('https://catch.astro.umd.edu/api/query/moving',
-                   params=params)
+base_url = "https://catch.astro.umd.edu/api"
+
+# API route for searches is .../catch
+res = requests.get(base_url + "/catch", params=params)
 
 # response is JSON formatted
 data = res.json()
@@ -34,26 +40,30 @@ data = res.json()
 # If 'queued' is True, listen to the CATCH event stream until a message
 # with our job ID prefix (first 8 characters) is 'success' or 'error'.
 if data['queued']:
-    messages = SSEClient('https://catch.astro.umd.edu/api/stream')
+    messages = SSEClient(base_url + "/stream")
     for message in messages:
+        # ignore blank lines
+        if message.data == "":
+            continue
+
         message_data = json.loads(message.data)
 
         # edit out keep-alive messages
         if not isinstance(message_data, dict):
             continue
 
-        if message_data['job_prefix'] == data['job_id'][:8]:
+        if message_data["job_prefix"] == data["job_id"][:8]:
             # this message is for us, print the text
             print(message_data['text'], file=sys.stderr)
         else:
             continue
 
-        # Message status may be 'success', 'error', 'running', 'queued'.
-        if message_data['status'] in ['error', 'success']:
+        # Message status may be "success", "error", "running", "queued".
+        if message_data["status"] in ["error", "success"]:
             break
 
-# 'results' is the URL to the search results
-res = requests.get(data['results'])
+# "results" is the URL to the search results
+res = requests.get(data["results"])
 
 # response is JSON formatted
 data = res.json()
